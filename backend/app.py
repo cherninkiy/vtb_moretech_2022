40e7661c72e1214f4f499aafc0ea1d854294ee92
cherninkiy <cherninkiy@mail.ru>
2022-10-05 16:00:23 +0300

    Backend changes

--- backend/app.py
+++ backend/app.py
@@ -5,20 +5,20 @@ import streamlit as st
 
 st.set_page_config(page_title="[NKotC] MORE.Tech Lenta")
 
-st.markdown(""" <style> 
-        #MainMenu {visibility: hidden;} 
-        footer {visibility: hidden;} 
-        </style> """, unsafe_allow_html=True)
+# st.markdown(""" <style> 
+#         #MainMenu {visibility: hidden;} 
+#         footer {visibility: hidden;} 
+#         </style> """, unsafe_allow_html=True)
 
-padding = 0
+# padding = 0
 
-st.markdown(f""" <style>
-    .reportview-container .main .block-container{{
-        padding-top: {padding}rem;
-        padding-right: {padding}rem;
-        padding-left: {padding}rem;
-        padding-bottom: {padding}rem;
-    }} </style> """, unsafe_allow_html=True)
+# st.markdown(f""" <style>
+#     .reportview-container .main .block-container{{
+#         padding-top: {padding}rem;
+#         padding-right: {padding}rem;
+#         padding-left: {padding}rem;
+#         padding-bottom: {padding}rem;
+#     }} </style> """, unsafe_allow_html=True)
 
 
 st.write("New Kids on the Court")
