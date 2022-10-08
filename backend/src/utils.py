b7911fd73750f3da49ba52d4d397602e299dc026
cherninkiy <cherninkiy@mail.ru>
2022-10-08 22:26:14 +0300

    Added Trends backend

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,10 +1,10 @@
+import sys
 import random
+import logging
 from .entities.app_params import NewsData, ArticleData
 
 
-def load_model():
-    model = {}
-    return model
+logger = logging.getLogger("uvicorn")
 
 
 def make_preds(model, user_id):
@@ -14,3 +14,23 @@ def make_preds(model, user_id):
 def get_content(article_id):
     return ArticleData.from_id(article_id)
 
+
+def setup_logger(logger):
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+def get_trends(topic_preds, topic_trends, date):
+    trends = topic_trends.loc[topic_trends['date'] == date, 'trend']
+    if trends.shape[0] == 0:
+        return {}
+    trends = trends.values[0]
+    mask1 = topic_preds['topic'].apply(lambda x: len(trends & x) > 0)
+    mask2 = topic_preds['date'].dt.day == date.day
+    news = topic_preds.loc[mask1 & mask2, ['title', 'orig_title', 'url', 'topic']].head(5).copy()
+    news['topic'] = news['topic'].apply(' '.join)
+    news = news.rename(columns={'title': 'keywords', 'orig_title': 'title'})
+    return news.to_dict(orient='index')
